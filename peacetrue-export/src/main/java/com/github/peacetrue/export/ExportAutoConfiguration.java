cda944aabb89bc12d417d32d544d270b3c1d5682
xiayx <xiayouxue@hotmail.com>
2019-01-03 21:55:24 +0800

    导出

--- peacetrue-export/src/main/java/com/github/peacetrue/export/ExportAutoConfiguration.java
+++ peacetrue-export/src/main/java/com/github/peacetrue/export/ExportAutoConfiguration.java
@@ -3,10 +3,12 @@ package com.github.peacetrue.export;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author xiayx
  */
+@Configuration
 @EnableConfigurationProperties(ExportProperties.class)
 public class ExportAutoConfiguration {
 
