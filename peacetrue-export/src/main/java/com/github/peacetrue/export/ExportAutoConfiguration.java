0a99833713ef477010ae8782f5716a41b00a3da2
xiayx <xiayouxue@hotmail.com>
2019-01-03 21:36:34 +0800

    导出

---
+++ peacetrue-export/src/main/java/com/github/peacetrue/export/ExportAutoConfiguration.java
@@ -0,0 +1,24 @@
+package com.github.peacetrue.export;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author xiayx
+ */
+@EnableConfigurationProperties(ExportProperties.class)
+public class ExportAutoConfiguration {
+
+    private ExportProperties exportProperties;
+
+    public ExportAutoConfiguration(ExportProperties exportProperties) {
+        this.exportProperties = exportProperties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(ExportService.class)
+    public ExportService exportService() {
+        return new CSVExportService(exportProperties.getDefaultContent());
+    }
+}
