5822278d8c0954a940539adbe4dbda2769295176
xiayx <xiayouxue@hotmail.com>
2019-03-15 16:08:05 +0800

    可序列化

--- peacetrue-core/src/main/java/com/github/peacetrue/core/OperatorCapableImpl.java
+++ peacetrue-core/src/main/java/com/github/peacetrue/core/OperatorCapableImpl.java
@@ -1,9 +1,13 @@
 package com.github.peacetrue.core;
 
+import java.io.Serializable;
+
 /**
  * @author xiayx
  */
-public class OperatorCapableImpl<Id> implements OperatorCapable<Id> {
+public class OperatorCapableImpl<Id> implements OperatorCapable<Id>, Serializable {
+
+    private static final long serialVersionUID = 0L;
 
     private Id operatorId;
     private String operatorName;
