1a3cc627cd9b19b867faa8bdfd143269bdb7ba97
xiayx <xiayouxue@hotmail.com>
2019-08-31 19:45:33 +0800

    添加其他通用方法

--- peacetrue-mybatis/src/main/java/com/github/peacetrue/mybatis/mapper/CommonMapper.java
+++ peacetrue-mybatis/src/main/java/com/github/peacetrue/mybatis/mapper/CommonMapper.java
@@ -1,8 +1,17 @@
 package com.github.peacetrue.mybatis.mapper;
 
-import org.apache.ibatis.annotations.ResultType;
-import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.*;
+import org.mybatis.dynamic.sql.SqlBuilder;
+import org.mybatis.dynamic.sql.SqlTable;
+import org.mybatis.dynamic.sql.delete.DeleteDSL;
+import org.mybatis.dynamic.sql.delete.MyBatis3DeleteModelAdapter;
+import org.mybatis.dynamic.sql.delete.render.DeleteStatementProvider;
+import org.mybatis.dynamic.sql.insert.render.InsertStatementProvider;
+import org.mybatis.dynamic.sql.select.MyBatis3SelectModelAdapter;
+import org.mybatis.dynamic.sql.select.QueryExpressionDSL;
+import org.mybatis.dynamic.sql.select.SelectDSL;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
+import org.mybatis.dynamic.sql.update.render.UpdateStatementProvider;
 import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
 
 import java.util.List;
@@ -32,5 +41,26 @@ public interface CommonMapper {
     @ResultType(Map.class)
     @SelectProvider(type = SqlProviderAdapter.class, method = "select")
     List<Map<String, Object>> selectListMap(SelectStatementProvider selectStatement);
+
+    @SelectProvider(type = SqlProviderAdapter.class, method = "select")
+    long count(SelectStatementProvider selectStatement);
+
+    @DeleteProvider(type = SqlProviderAdapter.class, method = "delete")
+    int delete(DeleteStatementProvider deleteStatement);
+
+    @InsertProvider(type = SqlProviderAdapter.class, method = "insert")
+    @Options(useGeneratedKeys = true, keyProperty = "record.id")
+    int insert(InsertStatementProvider<?> insertStatement);
+
+    @UpdateProvider(type = SqlProviderAdapter.class, method = "update")
+    int update(UpdateStatementProvider updateStatement);
+
+    default QueryExpressionDSL<MyBatis3SelectModelAdapter<Long>> countByExample(SqlTable sqlTable) {
+        return SelectDSL.selectWithMapper(this::count, SqlBuilder.count()).from(sqlTable);
+    }
+
+    default DeleteDSL<MyBatis3DeleteModelAdapter<Integer>> deleteByExample(SqlTable sqlTable) {
+        return DeleteDSL.deleteFromWithMapper(this::delete, sqlTable);
+    }
 
 }
